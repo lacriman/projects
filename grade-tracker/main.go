bf4ce9ce1e8e7db1d29d7df626f0d4e441f14232
lacriman <proskurin.yarik@gmail.com>
2025-04-21 17:58:28 +0200

    feat: add loop to calculate and print average grade

--- grade-tracker/main.go
+++ grade-tracker/main.go
@@ -15,7 +15,7 @@ func main() {
 			n, err := fmt.Scan(&usersGrade)
 
 			if n != 1 || err != nil || usersGrade < 0 {
-				fmt.Println("Invalid input! Please enter a **positive integer only**.")
+				fmt.Println("Invalid input! Please enter a positive integer only.")
 
 				var junk string 
 				fmt.Scanln(&junk)
@@ -26,5 +26,13 @@ func main() {
 			break
 		}
 	}
-	fmt.Println(grades)
+	averageGrade := 0
+	// maxGrade := 0
+	// minGrade := 0
+	gradesSum := 0
+	for i := 0; i < len(grades); i++ {
+		gradesSum += grades[i] 
+		averageGrade = gradesSum / len(grades)
+	}
+	fmt.Println(averageGrade)
 }
