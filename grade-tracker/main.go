c037dbc97f9c54ec3f32351a20ab8a87e0f7ce10
lacriman <proskurin.yarik@gmail.com>
2025-04-23 21:34:12 +0200

    feat: implement functions to find min and max grade

--- grade-tracker/main.go
+++ grade-tracker/main.go
@@ -14,7 +14,7 @@ func main() {
 			fmt.Printf("Grade number %d: ", i)
 			n, err := fmt.Scan(&usersGrade)
 
-			if n != 1 || err != nil || usersGrade < 0 {
+			if n != 1 || err != nil || usersGrade <= 0 {
 				fmt.Println("Invalid input! Please enter a positive integer only.")
 
 				var junk string 
@@ -27,12 +27,29 @@ func main() {
 		}
 	}
 	averageGrade := 0
-	// maxGrade := 0
-	// minGrade := 0
+	maxGrade := grades[0]
+	minGrade := grades[0]
 	gradesSum := 0
+	
+	// Find an average grade
 	for i := 0; i < len(grades); i++ {
 		gradesSum += grades[i] 
 		averageGrade = gradesSum / len(grades)
 	}
-	fmt.Println(averageGrade)
+	
+	// Find the max grade
+	for _, grade := range grades[1:] {
+		if grade > maxGrade {
+			maxGrade = grade
+		}
+	}
+	
+	// Find the min grade
+	for _, grade := range grades[1:] {
+		if grade < minGrade {
+			minGrade = grade
+		}
+	}
+
+	fmt.Printf("Your average grade is: %d, your max grade is: %d, and your lowest grade is: %d", averageGrade, maxGrade, minGrade)
 }
