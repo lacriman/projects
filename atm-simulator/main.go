f7cc0241da0279896793062d1a7d16edc901aabe
lacriman <proskurin.yarik@gmail.com>
2025-04-23 23:07:49 +0200

    feat: add checkPin function with 3-attempt limit

--- atm-simulator/main.go
+++ atm-simulator/main.go
@@ -9,17 +9,38 @@ import (
 )
 
 func main() {
-	// pin := 1234
-	// attemps := 0
-
 	fmt.Println("Welcome to the ATM")
 
-	balance := getValidNum("Write your balance: ")
+	checkPin()
+	balance := readPositiveInt("Write your balance: ")
 
 	fmt.Printf("Your balance is: %d", balance)
 }
 
-func getValidNum(message string) int {
+func checkPin() {
+	pin := 1234
+	attempts := 3
+
+	for attempts > 0 {
+		guess := readPositiveInt("Please write your PIN code: ")
+
+		if guess == pin {
+			fmt.Println("PIN correct.")
+			return
+		}
+		attempts--
+		if attempts == 0 {
+			fmt.Println("You have no attempts left, the card is blocked.")
+			os.Exit(1)
+		} else {
+			fmt.Printf("Wrong! You have %d more attempts. \n", attempts)
+		}
+
+	}
+
+}
+
+func readPositiveInt (message string) int {
 	scanner := bufio.NewScanner(os.Stdin)
 	for {
 		fmt.Print(message)
@@ -29,7 +50,7 @@ func getValidNum(message string) int {
 
 		if err != nil || num <= 0 {
 			fmt.Println("Wrong input, please write a positive number.")
-			continue			
+			continue
 		}
 
 		return num
