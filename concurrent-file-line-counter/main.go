60c4062dd874e36a8b7c5955ab6c6f7207b5a1b9
lacriman <proskurin.yarik@gmail.com>
2025-04-30 20:15:30 +0200

    feat: add file line counter using goroutines, WaitGroup, and os.Args

--- concurrent-file-line-counter/main.go
+++ concurrent-file-line-counter/main.go
@@ -1,5 +1,37 @@
 package main
 
+import (
+	"fmt"
+	"os"
+	"strings"
+	"sync"
+)
+
 func main() {
+	var wg sync.WaitGroup
+	files := os.Args[1:]
 	
+	for _, file := range files {
+		file:= file
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			err:=lineCounter(file)
+			if err != nil {
+			  fmt.Printf("error: %v\n", err)
+			}
+		}()
+	}
+
+	wg.Wait()
+}
+
+func lineCounter(file string) error{
+	data, err := os.ReadFile(file)
+	if err != nil {
+		return fmt.Errorf("Can't find a file %s", file)
+	}
+	lines := strings.Split(string(data), "\n")
+	fmt.Printf("Number of lines in file %s: %d\n",file, len(lines))
+	return	nil
 }
